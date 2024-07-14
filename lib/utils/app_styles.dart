4d823fbfa2cee5d0b48172cac8edcf36fa6375df
ahmed-shorbagy <a7med.shorbagy@gmail.com>
2024-07-14 13:53:59 +0300

    adding text styles and images added the font family and generated app assets class

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const String fontFamily = 'Roboto';
+  static TextStyle regular14 = const TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+  static TextStyle regular12 = const TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  );
+  static TextStyle regular10 = const TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w400,
+    fontSize: 10,
+  );
+  static TextStyle medium14 = const TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+  static TextStyle medium18 = const TextStyle(
+    fontFamily: fontFamily,
+    fontWeight: FontWeight.w500,
+    fontSize: 18,
+  );
+}
---
+++ lib/utils/assets.dart
@@ -0,0 +1,24 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesNotifications
+  /// assets/images/notifications.svg
+  static const String imagesNotifications = "assets/images/notifications.svg";
+
+  /// Assets for imagesProfileAvatar
+  /// assets/images/profileAvatar.svg
+  static const String imagesProfileAvatar = "assets/images/profileAvatar.svg";
+
+  /// Assets for imagesProject1
+  /// assets/images/project1.svg
+  static const String imagesProject1 = "assets/images/project1.svg";
+
+  /// Assets for imagesProject2
+  /// assets/images/project2.svg
+  static const String imagesProject2 = "assets/images/project2.svg";
+
+  /// Assets for imagesRegistrationBackGround
+  /// assets/images/registrationBackGround.svg
+  static const String imagesRegistrationBackGround = "assets/images/registrationBackGround.svg";
+}
+
