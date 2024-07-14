b6737479493936d316e03bc1f7edc6840eb8586d
ahmed-shorbagy <a7med.shorbagy@gmail.com>
2024-07-14 13:18:32 +0300

    Add registration module
    Added a new registration module with its corresponding controller, view, and binding. Also, updated the app routes to include the registration page.

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 
+import '../modules/Registration/bindings/registration_binding.dart';
+import '../modules/Registration/views/registration_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 
@@ -15,6 +17,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTRATION,
+      page: () => const RegistrationView(),
+      binding: RegistrationBinding(),
     ),
   ];
 }
